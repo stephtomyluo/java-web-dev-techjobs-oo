c108c8455f8cb9897f0eae7ba93de43e5e8efe93
Stephanie <4.struong@kscholars.org>
2020-02-24 18:51:11 -0600

    location, corecompetency, positiontype class

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,4 +1,5 @@
 package org.launchcode.techjobs_oo;
+import java.util.Objects;
 
 public class PositionType {
     private int id;
@@ -15,7 +16,23 @@ public class PositionType {
         this.value = value;
     }
 
-    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+    @Override
+
+    public String toString() {return value;};
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(!(o instanceof PositionType)) return false;
+        PositionType positionType = (PositionType) o;
+        return getId() == positionType.getId();
+
+    }
 
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.

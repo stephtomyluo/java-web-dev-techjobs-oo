d59a80bb1cd693b33c34516931fdc66ba1dd997f
Stephanie <4.struong@kscholars.org>
2020-03-15 23:06:37 -0500

    DRY & JobField

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,53 +1,8 @@
 package org.launchcode.techjobs_oo;
 import java.util.Objects;
 
-public class PositionType {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        id = nextId;
-        nextId++;
-    }
-
+public class PositionType extends JobField{
     public PositionType(String value) {
-        this();
-        this.value = value;
-    }
-
-    @Override
-
-    public String toString() {return value;};
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, value);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if(!(o instanceof PositionType)) return false;
-        PositionType positionType = (PositionType) o;
-        return getId() == positionType.getId();
-
-    }
-
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+        super(value);
     }
 }
